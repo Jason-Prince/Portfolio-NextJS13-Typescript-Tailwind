6a4776491eddf3a5ef1a4d52a49af32c9430f9f2
Jason Prince <jasoncprince@gmail.com>
2023-04-24 12:41:59 -0600

    herosection finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import HeroSection from '@/components/HeroSection'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-2xl">Hello World</h1>
+    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+      <HeroSection />
     </main>
   )
 }
